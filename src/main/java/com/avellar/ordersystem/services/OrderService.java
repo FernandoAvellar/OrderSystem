e9343cc016a304172c3a0ea8771b0e83dae01745
Fernando O. de Avellar Jr <favellar@yahoo.com.br>
2024-06-27 22:07:27 -0300

    Order, Instant, ISO 8601

---
+++ src/main/java/com/avellar/ordersystem/services/OrderService.java
@@ -0,0 +1,27 @@
+package com.avellar.ordersystem.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.avellar.ordersystem.entities.Order;
+import com.avellar.ordersystem.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+
+	public List<Order> findAll() {
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
