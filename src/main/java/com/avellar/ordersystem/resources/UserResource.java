b508d2c55d05e288ae87b0274d1ff4df4198c5cd
Fernando O. de Avellar Jr <favellar@yahoo.com.br>
2024-06-27 21:10:26 -0300

    Service layer, component registration

--- src/main/java/com/avellar/ordersystem/resources/UserResource.java
+++ src/main/java/com/avellar/ordersystem/resources/UserResource.java
@@ -1,21 +1,34 @@
 package com.avellar.ordersystem.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.avellar.ordersystem.entities.User;
+import com.avellar.ordersystem.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-	
+
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user1 = new User(1L, "Maria de Lourdes", "maria@yahoo.com.br", "35998771212", "1234..@@6");
-		return ResponseEntity.ok().body(user1);
+	public ResponseEntity<List<User>> findAll() {
+		return ResponseEntity.ok().body(service.findAll());
 	}
-	
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
 
 }
